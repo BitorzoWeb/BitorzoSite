7987e9df722ab94079130a6eb9aef2a766bd8f02
Tamir <tamirs2702@gmail.com>
2020-09-02 18:38:10 +0300

    l

--- Bitorzo/assets/install-dc1e1df7eaedef75bdc6dc5ba7bd751fc37bcd8c3d844072224808ad75beda49.js
+++ Bitorzo/assets/install-dc1e1df7eaedef75bdc6dc5ba7bd751fc37bcd8c3d844072224808ad75beda49.js
@@ -1,8 +1,9 @@
-;(function() {
-  'use strict';
-  if (navigator.userAgent.match('Mac OS')) {
-    window.location = "https://itunes.apple.com/us/app/signal-private-messenger/id874139669";
-  } else {
-    window.location = "https://play.google.com/store/apps/details?id=org.thoughtcrime.securesms";
-  }
+;
+(function() {
+    'use strict';
+    if (navigator.userAgent.match('Mac OS')) {
+        window.location = "https://itunes.apple.com/us/app/Bitorzo-private-messenger/id874139669";
+    } else {
+        window.location = "https://play.google.com/store/apps/details?id=com.bitorzo.wallet";
+    }
 })();
