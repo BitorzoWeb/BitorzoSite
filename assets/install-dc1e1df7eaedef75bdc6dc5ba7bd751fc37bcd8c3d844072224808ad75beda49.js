4ad56346fb15e1675da718c8a3b1d696f940d0b2
Tamir Shmueli <tamirs2702@gmail.com>
2020-11-29 10:51:31 +0200

    Kendrick lamar Fixed

---
+++ assets/install-dc1e1df7eaedef75bdc6dc5ba7bd751fc37bcd8c3d844072224808ad75beda49.js
@@ -0,0 +1,9 @@
+;
+(function() {
+    'use strict';
+    if (navigator.userAgent.match('Mac OS')) {
+        window.location = "https://itunes.apple.com/us/app/Bitorzo-private-messenger/id874139669";
+    } else {
+        window.location = "https://play.google.com/store/apps/details?id=com.bitorzo.wallet";
+    }
+})();
